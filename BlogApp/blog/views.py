0f9e5120d545585ee8e0d1b8398703c486c7d58e
a.akbarnejad <a.akbarnejad@gmail.com>
2024-01-21 13:44:30 +0330

    post crud added

--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Post
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
